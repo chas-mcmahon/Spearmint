b1881d4867ebf2786df9712a20434c4a7c05fbee
Chas <student@appacademy.io>
2014-04-21 15:17:00 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spearmint::Application.config.secret_key_base = '748615c48aa6f71dd9d004d34cd019b2494d78b48b1e0a1a2ad3d903b79d2e11b4a42e09702603049402c17052141d9295fa1bfe260bd5c210a839d4424ab201'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Spearmint::Application.config.session_store :cookie_store, key: '_Spearmint_session'
